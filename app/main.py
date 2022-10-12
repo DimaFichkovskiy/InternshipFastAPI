76151efe707b067bd42406bc3ad7338314040b8a
DimaFichkovskiy <fichkovskiy123@gmail.com>
2022-10-12 19:25:43 +0300

    Added in docker-compose Postgres and Redis. Added asynchronous connection to database. Added by CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,33 @@
+import databases
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.config import DATABASE_URL
+
+db = databases.Database(DATABASE_URL)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:8000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.on_event("startup")
+async def startup():
+    await db.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await db.disconnect()
 
 
 @app.get('/')
